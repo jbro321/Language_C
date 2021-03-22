1139e6efb48f5015c59b31fb4e09a2dbf093f438
JP <jaehyung0321@gmail.com>
2021-03-22 22:28:47 +0900

    210322

---
+++ CodeUp/Basic_100/1021.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char data[51];
+    scanf("%s", data);
+    printf("%s", data);
+    return 0;
+}
---
+++ CodeUp/Basic_100/1026.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int h, m, s;
+    scanf("%d:%d:%d", &h, &m, &s);
+    printf("%d\n", m);
+
+    return 0;
+}
