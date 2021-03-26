26119de0674578a4d94effe8d835e8321ebc2db2
JP <jaehyung0321@gmail.com>
2021-03-26 18:27:40 +0900

    Study Language C

--- Language_C/CodeUp_C/Basic_100/1071.c
+++ Language_C/CodeUp_C/Basic_100/1071.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
 
-int main(void)
+int main()
 {
-    printf("Hello\n");
+    int n;
+
+ //레이블은 콜론(:)으로 끝내고, 일반적으로 들여쓰기를 하지 않는다.
+reload:
+
+    scanf("%d", &n);
+    if(n!=0)
+    {
+        printf("%d\n", n);
+        goto reload;
+    }
     return 0;
 }
+
+/* 주석 토글 Shift+Alt+A
+
+#include<stdio.h>
+
+int main()
+{
+	int n;
+        
+        again:
+	scanf("%d", &n);
+	printf("%d\n", n);
+
+	if(n != 0) 
+		goto again;
+
+	return 0;
+} */
